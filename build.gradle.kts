e5471c216fe10e57c46beb6156c3a2fa57a79ae5
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2025-02-11 18:34:04 +0100

    generate first batch

--- build.gradle.kts
+++ build.gradle.kts
@@ -130,19 +130,19 @@ File(rootProject.rootDir.path + "/src/main/yaml")
             description = "Launches batch experiments for $capitalizedName"
             maxHeapSize = "${minOf(heap.toInt(), Runtime.getRuntime().availableProcessors() * taskSize)}m"
             File("data").mkdirs()
-            args(
-                "--override",
-                """
-                launcher: {
-                    parameters: {
-                        batch: [ seed, spacing, error ],
-                        showProgress: true,
-                        autoStart: true,
-                        parallelism: $threadCount,
-                    }
-                }
-                """.trimIndent(),
-            )
+//            args(
+//                "--override",
+//                """
+//                launcher: {
+//                    parameters: {
+//                        batch: [ seed, spacing, error ],
+//                        showProgress: true,
+//                        autoStart: true,
+//                        parallelism: $threadCount,
+//                    }
+//                }
+//                """.trimIndent(),
+//            )
         }
         runAllBatch.dependsOn(batch)
     }
